8c8a0746050cddb1755bb23375d38aa43f64789f
BarnabedevWebmobile <barnabe.dwwm48@gmail.com>
2024-01-24 11:51:05 +0100

    AAAAAAAA

--- src/index.php
+++ src/index.php
@@ -42,8 +42,18 @@
         };
         ?>
       </select>
-      <button class ="bg-slate-400 dark:text-white px-2 ml-2 rounded-lg">filtrer</button>
+      <div class="relative py-8">
+          <div class="absolute inset-y-0 start-0 flex items-center  ps-3 pointer-events-none">
+              <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+              </svg>
+          </div>
+          <input type="search" name = "search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos...">
+          <button type="submit" class="text-white absolute end-2.5 bottom-10 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+      </div>
     </form>
+
+    
   </section>
 
   <section class = "w-full flex justify-center dark:text-white">
@@ -74,31 +84,49 @@
       <!-- zone de filtrage -->
           <?php   
           // Affichage (SELECT) :
-      
-        if(isset($_GET['categorie'],$_GET['categorie']) && $_GET['categorie'] !== "none" && $_GET['domaine'] !== "none"){
-          $result = $pdo->query("SELECT * FROM `favoris` INNER JOIN `cat_fav` ON favoris.id_fav=cat_fav.id_fav INNER JOIN `domaine` ON favoris.id_dom=domaine.id_dom INNER JOIN `categorie` ON cat_fav.id_cat=categorie.id_cat WHERE categorie.id_cat=".$_GET['categorie']." AND domaine.id_dom=".$_GET['domaine'].";");
+
+        if (isset($_GET['search'])){
+          $result = $pdo->query("SELECT * FROM `favoris` INNER JOIN `domaine` ON favoris.id_dom=domaine.id_dom WHERE libelle LIKE '%".$_GET['search']."%' ORDER BY `id_fav` ASC ;");
           $favoris = $result->fetchAll(PDO::FETCH_ASSOC); 
         }else{
-          if(isset($_GET['domaine']) && $_GET['domaine'] !== "none" && $_GET['categorie'] == "none"){
-            $result = $pdo->query("SELECT * FROM `favoris` INNER JOIN `domaine` ON favoris.id_dom=domaine.id_dom WHERE domaine.id_dom=".$_GET['domaine']." ORDER BY `id_fav` ASC;");
+          if(isset($_GET['categorie'],$_GET['domaine']) && $_GET['categorie'] !== "none" && $_GET['domaine'] !== "none"){
+            $result = $pdo->query("SELECT * FROM `favoris` INNER JOIN `cat_fav` ON favoris.id_fav=cat_fav.id_fav 
+            INNER JOIN `domaine` ON favoris.id_dom=domaine.id_dom 
+            INNER JOIN `categorie` ON cat_fav.id_cat=categorie.id_cat 
+            WHERE categorie.id_cat=".$_GET['categorie']." AND domaine.id_dom=".$_GET['domaine']." LIMIT ".$_GET['show'].";");
             $favoris = $result->fetchAll(PDO::FETCH_ASSOC); 
           }else{
-            if(isset($_GET['categorie']) && $_GET['categorie'] !== "none" && $_GET['domaine'] == "none"){
-              $result = $pdo->query("SELECT * FROM `favoris` INNER JOIN `cat_fav` ON favoris.id_fav=cat_fav.id_fav INNER JOIN `domaine` ON favoris.id_dom=domaine.id_dom INNER JOIN `categorie` ON cat_fav.id_cat=categorie.id_cat WHERE categorie.id_cat=".$_GET['categorie'].";");
+            if(isset($_GET['domaine']) && $_GET['domaine'] !== "none" && $_GET['categorie'] == "none"){
+              $result = $pdo->query("SELECT * FROM `favoris` INNER JOIN `domaine` ON favoris.id_dom=domaine.id_dom 
+              WHERE domaine.id_dom=".$_GET['domaine']." ORDER BY `id_fav` ASC LIMIT ".$_GET['show'].";");
               $favoris = $result->fetchAll(PDO::FETCH_ASSOC); 
             }else{
-              $result = $pdo->query("SELECT * FROM `favoris` INNER JOIN `domaine` ON favoris.id_dom=domaine.id_dom ORDER BY `id_fav` ASC;");
-              $favoris = $result->fetchAll(PDO::FETCH_ASSOC); 
+              if(isset($_GET['categorie']) && $_GET['categorie'] !== "none" && $_GET['domaine'] == "none"){
+                $result = $pdo->query("SELECT * FROM `favoris` INNER JOIN `cat_fav` ON favoris.id_fav=cat_fav.id_fav 
+                INNER JOIN `domaine` ON favoris.id_dom=domaine.id_dom 
+                INNER JOIN `categorie` ON cat_fav.id_cat=categorie.id_cat 
+                WHERE categorie.id_cat=".$_GET['categorie']." LIMIT ".$_GET['show'].";");
+                $favoris = $result->fetchAll(PDO::FETCH_ASSOC); 
+              }else{
+                if(isset($_GET['show'])){
+                  $result = $pdo->query("SELECT * FROM `favoris` INNER JOIN `domaine` ON favoris.id_dom=domaine.id_dom 
+                  ORDER BY `id_fav` ASC LIMIT ".$_GET['show'].";");
+                  $favoris = $result->fetchAll(PDO::FETCH_ASSOC); 
+                }else{
+                  $result = $pdo->query("SELECT * FROM `favoris` INNER JOIN `domaine` ON favoris.id_dom=domaine.id_dom ORDER BY `id_fav` ASC ;");
+                  $favoris = $result->fetchAll(PDO::FETCH_ASSOC); 
+                }
+              }
             }
-          }
-        }        
+          }        
+        }
     ?> 
     <!-- zone de filtrage -->
     <?php
     foreach($favoris as $favori){
     ?>
       <tr class="border-y-2 py-2 odd:bg-slate-600 even:bg-slate-700 hover:bg-slate-400">
-          <td class = "p-2">
+          <td class = "p-2 text-amber-400">
             <?php echo $favori['id_fav'] ?>
           </td>
           <td>
